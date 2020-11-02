ef64f1fac053d6e48eb81250b5cbe7580dbb2dda
Flonja <vanjavanessen@icloud.com>
2020-11-02 11:26:19 +0100

    Add WhiskyModel

---
+++ LiquorStore.DAL/Models/WhiskyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LiquorStore.DAL.Models
+{
+    public class WhiskyModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ProductionArea { get; set; }
+        public decimal AlcoholPercentage { get; set; }
+        public WhiskyKind Kind { get; set; }
+        public string LabelPath { get; set; }
+    }
+
+    public enum WhiskyKind
+    {
+        Blend,
+        [Display(Name = "Single Malt")] SingleMalt
+    }
+}
