c7c6e74473c44cd6ad15f204df858d638e909f58
Pieter <pieterbrandsen2@gmail.com>
2020-11-02 11:23:56 +0100

    Added application user

---
+++ LiquorStore.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiquorStore.DAL.Models
+{
+    class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HomeTown { get; set; }
+        public DateTime Age { get; set; }
+    }
+}
