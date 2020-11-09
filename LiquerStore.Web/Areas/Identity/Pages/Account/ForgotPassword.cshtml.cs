e1418f05aebed428f0f04e53a3f91233948187ac
Pieter <pieterbrandsen2@gmail.com>
2020-11-09 15:03:22 +0100

    Small change

--- LiquerStore.Web/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ LiquerStore.Web/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using LiquerStore.DAL.Models;
 
 namespace LiquerStore.Web.Areas.Identity.Pages.Account
 {
@@ -19,7 +20,7 @@ namespace LiquerStore.Web.Areas.Identity.Pages.Account
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IEmailSender _emailSender;
 
-        public ForgotPasswordModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        public ForgotPasswordModel(UserManager<ApplicationUser> userManager, IEmailSender emailSender)
         {
             _userManager = userManager;
             _emailSender = emailSender;
