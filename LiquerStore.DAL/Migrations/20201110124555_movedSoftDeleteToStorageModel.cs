d7f1dd6723b7f47070097c36a7efce78f5ec070b
Pieter <pieterbrandsen2@gmail.com>
2020-11-10 14:00:02 +0100

    Moved disable/enable to details page

---
+++ LiquerStore.DAL/Migrations/20201110124555_movedSoftDeleteToStorageModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LiquerStore.DAL.Migrations
+{
+    public partial class movedSoftDeleteToStorageModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoftDeleted",
+                table: "Whiskies");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SoftDeleted",
+                table: "Storages",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoftDeleted",
+                table: "Storages");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SoftDeleted",
+                table: "Whiskies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- LiquerStore.DAL/Models/StorageModel.cs
+++ LiquerStore.DAL/Models/StorageModel.cs
@@ -12,9 +12,12 @@ namespace LiquerStore.DAL.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Display(Name = "Aantal beschikbaar")]
         public int Available { get; set; }
+        [Display(Name = "Aantal gereserveerd")]
         public int Reserved { get; set; }
         public int WhiskyId { get; set; }
         public virtual WhiskyModel Whisky { get; set; }
+        public bool SoftDeleted { get; set; }
     }
 }
