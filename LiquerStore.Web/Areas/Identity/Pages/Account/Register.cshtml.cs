7c8b58cfc7669e1f622ad82d753aa2f8185d6228
Pieter <pieterbrandsen2@gmail.com>
2020-11-10 13:25:24 +0100

    Fixed bugs

--- LiquerStore.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LiquerStore.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,7 +70,7 @@ namespace LiquerStore.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Age = Input.Age, FirstName = Input.FirstName, LastName = Input.LastName, HomeTown = Input.HomeTown, };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
