a0b79e65df5682dfa63088e605e5c6c6d6828edc
Pieter <pieterbrandsen2@gmail.com>
2020-11-12 15:33:43 +0100

    TS added to wwwroot

---
+++ LiquerStore.Web/wwwroot/js/dataTables.js
@@ -0,0 +1,64 @@
+export var table = function (data) {
+    $(document).ready(function () {
+        $('#table').DataTable({
+            data: data,
+            processing: true,
+            serverSide: false,
+            // filter: true, // this is for disable filter (search box)
+            orderMulti: false,
+            columns: [
+                {
+                    "data": "Name",
+                    "render": function (data, type, full, meta) {
+                        return full.Whisky.Name;
+                    }
+                },
+                {
+                    "data": "Age",
+                    "render": function (data, type, full, meta) {
+                        return full.Whisky.Age;
+                    }
+                },
+                {
+                    "data": "ProductionArea",
+                    "render": function (data, type, full, meta) {
+                        return full.Whisky.ProductionArea;
+                    }
+                },
+                {
+                    "data": "AlcoholPercentage",
+                    "render": function (data, type, full, meta) {
+                        return full.Whisky.AlcoholPercentage;
+                    }
+                },
+                {
+                    "data": "Kind",
+                    "render": function (data, type, full, meta) {
+                        return full.Whisky.Kind;
+                    }
+                },
+                {
+                    "data": "Available",
+                    "render": function (data, type, full, meta) {
+                        return full.Available;
+                    }
+                },
+                {
+                    "render": function (data, type, full, meta) {
+                        return "<a class=\"btn btn-info\" href=\"/Liquers/Edit?Id=" + full.Id + "\">Aanpassen</a>";
+                    }
+                },
+                {
+                    "render": function (data, type, full, meta) {
+                        return "<a class=\"btn btn-warning\" href=\"/Liquers/Details?Id=" + full.Id + "\">Alle gegevens</a>";
+                    }
+                },
+                {
+                    "render": function (data, type, full, meta) {
+                        return (full.Whisky.SoftDelete) ? "Actief" : "Uitgezet";
+                    }
+                },
+            ]
+        });
+    });
+};
