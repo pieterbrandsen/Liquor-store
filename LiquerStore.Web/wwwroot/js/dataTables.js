f79fed90b391e8727c3e43d023eada76c62a24da
Pieter <pieterbrandsen2@gmail.com>
2020-11-16 10:53:39 +0100

    Ts bug-fixes

--- LiquerStore.Web/wwwroot/js/dataTables.js
+++ LiquerStore.Web/wwwroot/js/dataTables.js
@@ -1,6 +1,6 @@
-export var dataTable = function (data) {
+export var dataTable = function (data, isAuthenticated) {
     $(document).ready(function () {
-        $('#table').DataTable({
+        $("#table").DataTable({
             data: data,
             processing: true,
             serverSide: false,
@@ -8,57 +8,62 @@ export var dataTable = function (data) {
             orderMulti: false,
             columns: [
                 {
-                    "data": "Name",
-                    "render": function (data, type, full, meta) {
+                    data: "Name",
+                    render: function (data, type, full, meta) {
                         return full.Whisky.Name;
-                    }
+                    },
                 },
                 {
-                    "data": "Age",
-                    "render": function (data, type, full, meta) {
+                    data: "Age",
+                    render: function (data, type, full, meta) {
                         return full.Whisky.Age;
-                    }
+                    },
                 },
                 {
-                    "data": "ProductionArea",
-                    "render": function (data, type, full, meta) {
+                    data: "ProductionArea",
+                    render: function (data, type, full, meta) {
                         return full.Whisky.ProductionArea;
-                    }
+                    },
                 },
                 {
-                    "data": "AlcoholPercentage",
-                    "render": function (data, type, full, meta) {
+                    data: "AlcoholPercentage",
+                    render: function (data, type, full, meta) {
                         return full.Whisky.AlcoholPercentage;
-                    }
+                    },
                 },
                 {
-                    "data": "Kind",
-                    "render": function (data, type, full, meta) {
+                    data: "Kind",
+                    render: function (data, type, full, meta) {
                         return full.Whisky.Kind;
-                    }
+                    },
                 },
                 {
-                    "data": "Available",
-                    "render": function (data, type, full, meta) {
+                    data: "Available",
+                    render: function (data, type, full, meta) {
                         return full.Available;
-                    }
+                    },
                 },
                 {
-                    "render": function (data, type, full, meta) {
+                    render: function (data, type, full, meta) {
                         return "<a class=\"btn btn-info\" href=\"/Liquers/Edit?Id=" + full.Id + "\">Aanpassen</a>";
-                    }
+                    },
                 },
                 {
-                    "render": function (data, type, full, meta) {
+                    render: function (data, type, full, meta) {
                         return "<a class=\"btn btn-warning\" href=\"/Liquers/Details?Id=" + full.Id + "\">Alle gegevens</a>";
-                    }
+                    },
                 },
                 {
-                    "render": function (data, type, full, meta) {
-                        return (full.Whisky.SoftDelete) ? "Actief" : "Uitgezet";
-                    }
+                    render: function (data, type, full, meta) {
+                        return full.Whisky.SoftDelete ? "Actief" : "Uitgezet";
+                    },
                 },
-            ]
+                {
+                    render: function (data, type, full, meta) {
+                        return "<a class=\"btn btn-success\" href=\"/Liquers/Reserve?Id=" + full.Id + "\">Reserveren</a>";
+                    },
+                },
+            ],
         });
     });
 };
