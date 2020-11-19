1a422fec00205675044a2172c36a724cd1e24d51
Pieter <pieterbrandsen2@gmail.com>
2020-11-19 15:00:35 +0100

    Push

---
+++ LiquerStore.Web/Pages/Reserve - Copy/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using LiquerStore.DAL.Models;
+using LiquerStore.DAL.Services.DbCommands;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LiquerStore.Web.Pages.Reserve
+{
+    public class IndexModel : PageModel
+    {
+        // Get the interface
+        private readonly IStorage _db;
+
+        public IndexModel(IStorage db)
+        {
+            // Save the interface to this interface
+            _db = db;
+        }
+
+        // Create a bindable variable to load on the page itself
+        public IList<StorageModel> ProductsActive { get; set; }
+
+        public void OnGet()
+        {
+            // Get all active products from DB
+            ProductsActive = _db.GetAllActiveWhiskies();
+        }
+    }
+}
