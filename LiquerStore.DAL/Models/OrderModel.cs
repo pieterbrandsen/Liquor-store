bdd89565896bef57b5645f47c9d81695cd46f439
Flonja <vanjavanessen@icloud.com>
2020-11-19 14:52:54 +0100

    ja

--- LiquerStore.DAL/Models/OrderModel.cs
+++ LiquerStore.DAL/Models/OrderModel.cs
@@ -9,7 +9,7 @@ namespace LiquerStore.DAL.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        public CustomerModel Customer { get; set; }
+        public ApplicationUser Customer { get; set; }
         public WhiskyModel Whisky { get; set; }
         public bool Completed { get; set; }
     }
