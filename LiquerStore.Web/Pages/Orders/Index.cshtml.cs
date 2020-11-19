ac887bfa593fbf3c1f9d7e835a21f5c47f64f09c
Pieter <pieterbrandsen2@gmail.com>
2020-11-19 15:00:45 +0100

    Push

--- LiquerStore.Web/Pages/Orders/Index.cshtml.cs
+++ LiquerStore.Web/Pages/Orders/Index.cshtml.cs
@@ -3,26 +3,26 @@ using LiquerStore.DAL.Models;
 using LiquerStore.DAL.Services.DbCommands;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LiquerStore.Web.Pages.Reserve
+namespace LiquerStore.Web.Pages.Order
 {
     public class IndexModel : PageModel
     {
         // Get the interface
-        private readonly IStorage _db;
+        private readonly IOrder _db;
 
-        public IndexModel(IStorage db)
+        public IndexModel(IOrder db)
         {
             // Save the interface to this interface
             _db = db;
         }
 
         // Create a bindable variable to load on the page itself
-        public IList<StorageModel> ProductsActive { get; set; }
+        public IList<StorageModel> OrdersActive { get; set; }
 
         public void OnGet()
         {
-            // Get all active products from DB
-            ProductsActive = _db.GetAllActiveWhiskies();
+            // Get all active orders from DB
+            OrdersActive = _db.GetAllActive();
         }
     }
 }
